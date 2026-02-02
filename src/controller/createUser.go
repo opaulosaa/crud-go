c832c7bb0f7904ba1617418333977d4b62797350
opaulosaa <pauloarneto@gmail.com>
2026-02-02 14:18:03 -0300

    fix: alterações necessárias para funcionamento das rotas

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/opaulosaa/crud-go/src/rest_err"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
+	c.JSON(err.Code, err)
+}
