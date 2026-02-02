396ee2aa593b5f0994dc61fb28ec6925468ca493
opaulosaa <pauloarneto@gmail.com>
2026-02-02 15:31:05 -0300

    feat: criação de request e response do usuário

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,21 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/opaulosaa/crud-go/src/controller/model/request"
 	"github.com/opaulosaa/crud-go/src/rest_err"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var UserRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(UserRequest)
 }
